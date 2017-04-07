c2420d6ee939361de54da1e48c7af3452af34ce8
Sreela Basu Roy <sroy19@sapient.com>
2017-04-07 18:54:57 +0530

    push item to mongodb

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongo = require('mongodb').MongoClient;
 
+var database;
+
 app.use(bodyParser.json());
 
 app.use(function(req,res,next){
@@ -15,12 +17,14 @@ app.use(function(req,res,next){
 
 app.post('/api/message',function(req,res){
 	console.log(req.body);
+	database.collection('messages').insertOne(req.body);
 	res.status(200);
 })
 
 mongo.connect("mongodb://localhost:27017/test",function(err,db){
 	if(!err){
 		console.log("we are connected to mongo");
+		database = db;
 	}
 })
 
